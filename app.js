9dae723ef4aed47f9796cd3afde06723df1a0e43
Nick Fox-Gieg <nick@fox-gieg.com>
2024-03-14 19:15:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,18 @@ let io, http, https;
 
 // Part of the setup complexity here is needed to make the app work both
 // on a local machine, and in a real deployment.
-if (!debug) {
+if (debug) {
+    http = require("http").Server(app);
+
+    io = require("socket.io")(http, { 
+        pingInterval: ping_interval,
+        pingTimeout: ping_timeout
+    });
+
+    http.listen(port_http, function() {
+        console.log("\nNode.js listening on http port " + port_http);
+    });
+} else {
     http = require("http");
 
     http.createServer(function(req, res) {
@@ -51,17 +62,6 @@ if (!debug) {
 
     https.listen(port_https, function() {
         console.log("\nNode.js listening on https port " + port_https);
-    });
-} else {
-    http = require("http").Server(app);
-
-    io = require("socket.io")(http, { 
-        pingInterval: ping_interval,
-        pingTimeout: ping_timeout
-    });
-
-    http.listen(port_http, function() {
-        console.log("\nNode.js listening on http port " + port_http);
     });
 }
 
